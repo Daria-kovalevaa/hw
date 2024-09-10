77aa903c6e791739737fce20fbadedfb44a2bc67
Daria-kovalevaa <lilak05@yandex.ru>
2024-09-10 11:37:34 +0300

    second commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,12 +11,13 @@ let dice1 = Math.floor(Math.random() * 7)// your code
 let dice2 = Math.floor(Math.random() * 7) // your code
 
 
-let isWinningDouble  // your code
+let isWinningDouble = true // your code
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (dice1 > 3 === dice2 > 3) {
-  isWinningDouble = true
+
+if ((dice1 === dice2)> 3) {
+  isWinningDouble
   console.log('Выигрышный дубль!')
 } else {
   isWinningDouble = false
