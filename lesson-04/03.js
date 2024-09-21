0211955c54f826c2cb2a40822be34e0abd50898e
Daria-kovalevaa <lilak05@yandex.ru>
2024-09-21 11:01:05 +0300

    second commit

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,11 +17,23 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements(array1,array2) {
-    let result = []
-    for (let i = 0; i < array1.length -1; i ++) {
-        if (array2.includes(array1[i])){
-        result.push(array1[i])
+// function findCommonElements(array1,array2) {
+//     let result = []
+//     for (let i = 0; i < array1.length -1; i ++) {
+//         if (array2.includes(array1[i])){
+//         result.push(array1[i])
+//         }
+//     } return result
+//   }
+
+function findCommonElements(array1, array2) {
+    const mainArray = []
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j] && array1[i] !== 'undefined') {
+                mainArray.push(array1[i]);
+            }
         }
-    } return result
-  }
+    }
+    return mainArray;
+}
